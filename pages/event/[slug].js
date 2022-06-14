19fb6fc4baadbae358f54955930d912a5680846a
Franco Ortega <francopdx@gmail.com>
2022-06-14 15:54:47 -0700

    add server side rendering to dynamic slug page for Events

--- pages/event/[slug].js
+++ pages/event/[slug].js
@@ -1,16 +1,52 @@
-import { useRouter } from "next/router";
-import styles from './slug.module.css'
+import styles from './slug.module.css';
 
-const Event = () => {
-  const router = useRouter();
-  const { slug } = router.query;
+const Event = ({ currentSlug }) => {
 
   return (
-    <div className={styles.slug}>
+    <div className={styles.SlugStyles}>
       <h1>Event Page</h1>
-      <p>This is the {slug && slug.charAt(0).toUpperCase() + slug.substring(1)} page of Event.</p>
+      <p>
+        This is the{' '}
+        {currentSlug.slug &&
+          currentSlug.slug.charAt(0).toUpperCase() + currentSlug.slug.substring(1)}{' '}
+        page of Event.
+      </p>
     </div>
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    fallback: false,
+    paths: [
+      {
+        params: {
+          slug: 'one',
+        },
+      },
+      {
+        params: {
+          slug: 'two',
+        },
+      },
+      {
+        params: {
+          slug: 'three',
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  const activeLink = await context.params;
+  console.log(activeLink);
+
+  return {
+    props: {
+      currentSlug: activeLink
+    },
+  };
+}
 
 export default Event;
