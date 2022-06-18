e8ba7114b401e88340ab902311fa47345cc34e3c
Franco Ortega <francopdx@gmail.com>
2022-06-17 19:53:28 -0700

    add lorem content and minor styling

--- pages/api/stuff.js
+++ pages/api/stuff.js
@@ -1,5 +1,5 @@
 export default function handler (req, res) {
   res.status(200).json({
-    content: 'This is the page of Stuff.'
+    content: 'This is the page of Stuff. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, omnis harum necessitatibus dignissimos odit doloribus non repudiandae assumenda distinctio, cupiditate dolores aut ullam, perferendis animi veritatis nemo quasi adipisci voluptatibus nam repellat sed mollitia nobis ducimus! Autem laborum adipisci ducimus alias perferendis nihil assumenda fugiat vero. Sunt beatae maxime ipsa.'
   });
 };
